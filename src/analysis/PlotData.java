a54b417fdf4ae03037c6c5fa41db33d7056c08dc
Shreyyas <Shreyyas@143.215.127.166>
2014-09-06 00:27:23 -0400

    Fixed Number of Data Points Issue. Need to integrate normalizing running time array now

--- src/analysis/PlotData.java
+++ src/analysis/PlotData.java
@@ -1,70 +1,78 @@
 package analysis;
 
+import java.util.ArrayList;
+
 public class PlotData 
 {
 	public static final int ROW    = 2;
 	public static final int COLUMN = 6;
 	
-	public static int NUM_NODES; 
+	public static int NUM_DATA_PTS; 
 	public static int STARTING_VALUE;
 	public static long STOPPING_VALUE;
 	
 	private LinkedList list;
-	private double[] numberOfNodes;
 	private double[] normalizedRunningTime;
 	
-	private PlotData(int numNodes, int startingValue, long stoppingValue) 
+	private PlotData(int startingValue, long stoppingValue) 
 	{
-		NUM_NODES			  = numNodes;
 		STARTING_VALUE 		  = startingValue;
-		STOPPING_VALUE 		  = stoppingValue;
-		
+		STOPPING_VALUE 		  = stoppingValue;		
 		list           	 	  = new LinkedList();
-		numberOfNodes         = new double[NUM_NODES];
-		normalizedRunningTime = new double[NUM_NODES];
 	}
-	
+
 	public double[] calculateNodesArray() {
 		int value = STARTING_VALUE;
-		int powerOf2 = 16;
-		int i;
+		int powerOf2 = 2;
+		ArrayList<Double> arrayList = new ArrayList<Double>();
+		arrayList.add((double) STARTING_VALUE);
 		
-		for (i = 0; STOPPING_VALUE > value; i++) {
-			System.out.println("i: "+i);
+		while(STOPPING_VALUE >= powerOf2)
+		{
 			System.out.println("Value: "+ value);
 			System.out.println("PowerOf2: "+powerOf2);
 			
-			while(value > powerOf2 && STOPPING_VALUE > powerOf2) {
+			while(value >= powerOf2) {
 				powerOf2 *= 2;
 			}
-			numberOfNodes[i] = value;
+			if(STOPPING_VALUE < value) {
+				System.out.println("STOPPINGVAL < VALUE");
+				arrayList.add((double) STOPPING_VALUE);
+			}
+			
 			value = powerOf2;
+			if(STOPPING_VALUE > value) {
+				arrayList.add((double) value);
+			}
 			powerOf2 *= 2;
+			
+			if(STOPPING_VALUE > value && STOPPING_VALUE <= powerOf2) {
+				arrayList.add((double) STOPPING_VALUE);
+			}
 		}
-		numberOfNodes[NUM_NODES-1] = STOPPING_VALUE;
-		for(int j = 0; j < numberOfNodes.length; j++)
+		for(int i = 0; i < arrayList.size(); i++) 
 		{
-				System.out.println(numberOfNodes[j]);
+			System.out.println(arrayList.get(i));
 		}
-		return numberOfNodes;
+		NUM_DATA_PTS = arrayList.size();
+		return convertToArray(arrayList);
 	}
-	public double[] calculateTraversalTime() 
+	
+	public double[] calculateTraversalTime(int index, long node) 
 	{	
-		for(int i = 1; i <= NUM_NODES; i++) {
+		for(int i = 1; i <= node; i++) {
 			list.add(i);
 		}
 				
 		double startTime = System.nanoTime();
-		Node finalNode = list.traverse(NUM_NODES);
+		Node finalNode = list.traverse(node);
 		double endTime = System.nanoTime();
 		
 		double estimatedRunTime = endTime - startTime;
-		double normalizingRunningTime = estimatedRunTime/NUM_NODES;
+		double normalizingRunningTime = estimatedRunTime/node;
 		
-		for(int i = 0; i < NUM_NODES; i++) {
-			normalizedRunningTime[i] = normalizingRunningTime;
-		}
-		 ///NEEDEED TO FIXX FOR EVERY TIME IT RUNS
+		normalizedRunningTime[index] = normalizingRunningTime;
+			///NEEDEED TO FIXX FOR EVERY TIME IT RUNS
 		//Need to plot the data for normalizingRunningTime for numLinks varying from 25, 32, 64, 128, 224
 		
 		System.out.println("I've traversed to the node with value of: " +finalNode.getData());
@@ -76,15 +84,24 @@ public class PlotData
 		return normalizedRunningTime;
 	}
 	
-	public long getNumNodes() {
-		return NUM_NODES;
+	public double[] convertToArray(ArrayList<Double> arrayList) {
+		double[] result = new double[arrayList.size()];
+    	int i = 0;
+    	for(Double d : arrayList) {
+    		result[i++] = d.doubleValue();
+    	}
+    	return result;
 	}
-	public void setNumNodes(int value) {
-		NUM_NODES = value;
+	
+	public long getNumDataPoints() {
+		return NUM_DATA_PTS;
+	}
+	public void setNumDataPoints(int value) {
+		NUM_DATA_PTS = value;
 	}
 	
 	public static void main(String[] args) {
-		PlotData data = new PlotData(6, 25, 390);
+		PlotData data = new PlotData(25, 254);
 		data.calculateNodesArray();
 	}
 }
