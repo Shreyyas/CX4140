e5f46b9c74fe46fe301d5d0607c90e2c9f14683a
Shreyyas <Shreyyas@143.215.127.166>
2014-09-06 01:44:37 -0400

    Normalizing Running Time decreases with an increase in the number of nodes.

--- src/analysis/PlotData.java
+++ src/analysis/PlotData.java
@@ -12,20 +12,20 @@ public class PlotData
 	public static long STOPPING_VALUE;
 	
 	private LinkedList list;
+	private long[] numberOfNodes;
 	private double[] normalizedRunningTime;
 	
 	private PlotData(int startingValue, long stoppingValue) 
 	{
 		STARTING_VALUE 		  = startingValue;
 		STOPPING_VALUE 		  = stoppingValue;		
-		list           	 	  = new LinkedList();
 	}
 
-	public double[] calculateNodesArray() {
+	public long[] calculateNodesArray() {
 		int value = STARTING_VALUE;
 		int powerOf2 = 2;
-		ArrayList<Double> arrayList = new ArrayList<Double>();
-		arrayList.add((double) STARTING_VALUE);
+		ArrayList<Long> arrayList = new ArrayList<Long>();
+		arrayList.add((long) STARTING_VALUE);
 		
 		while(STOPPING_VALUE >= powerOf2)
 		{
@@ -37,17 +37,17 @@ public class PlotData
 			}
 			if(STOPPING_VALUE < value) {
 				System.out.println("STOPPINGVAL < VALUE");
-				arrayList.add((double) STOPPING_VALUE);
+				arrayList.add((long) STOPPING_VALUE);
 			}
 			
 			value = powerOf2;
 			if(STOPPING_VALUE > value) {
-				arrayList.add((double) value);
+				arrayList.add((long) value);
 			}
 			powerOf2 *= 2;
 			
 			if(STOPPING_VALUE > value && STOPPING_VALUE <= powerOf2) {
-				arrayList.add((double) STOPPING_VALUE);
+				arrayList.add((long) STOPPING_VALUE);
 			}
 		}
 		for(int i = 0; i < arrayList.size(); i++) 
@@ -55,40 +55,60 @@ public class PlotData
 			System.out.println(arrayList.get(i));
 		}
 		NUM_DATA_PTS = arrayList.size();
-		return convertToArray(arrayList);
+		return convertToLongArray(arrayList);
 	}
 	
-	public double[] calculateTraversalTime(int index, long node) 
+	public double[] calculateTraversalTime() 
 	{	
-		for(int i = 1; i <= node; i++) {
-			list.add(i);
+		double startTime;
+		double endTime;
+		double estimatedRunTime;
+		double normalizingRunningTime;
+		Node finalNode;
+
+		numberOfNodes = calculateNodesArray();
+		int lengthOfNodesArray = numberOfNodes.length;
+		ArrayList<Double> runningTimeList = new ArrayList<Double>();
+		
+		for(int i = 0; i < lengthOfNodesArray; i++)
+		{
+			for(int j = 1; j <= numberOfNodes[i]; j++) {
+				list = new LinkedList();
+				list.add(j);
+			}
+			startTime = System.nanoTime();
+			finalNode = list.traverse(numberOfNodes[i]);
+			endTime = System.nanoTime();
+		
+			estimatedRunTime = endTime - startTime;
+			normalizingRunningTime = estimatedRunTime/numberOfNodes[i];
+		
+			runningTimeList.add((double) normalizingRunningTime);
+			
+			System.out.println("I've traversed to the node with value of: " +finalNode.getData());
+			System.out.println("Start Time: " +startTime/100000); 
+			System.out.println("End Time: " +endTime/100000);
+			System.out.println("Estimated Run Time: " +estimatedRunTime/100000);
+			System.out.println("Normalizing Run Time: " +normalizingRunningTime);
 		}
-				
-		double startTime = System.nanoTime();
-		Node finalNode = list.traverse(node);
-		double endTime = System.nanoTime();
-		
-		double estimatedRunTime = endTime - startTime;
-		double normalizingRunningTime = estimatedRunTime/node;
-		
-		normalizedRunningTime[index] = normalizingRunningTime;
-			///NEEDEED TO FIXX FOR EVERY TIME IT RUNS
-		//Need to plot the data for normalizingRunningTime for numLinks varying from 25, 32, 64, 128, 224
-		
-		System.out.println("I've traversed to the node with value of: " +finalNode.getData());
-		System.out.println("Start Time: " +startTime/1000); 
-		System.out.println("End Time: " +endTime/1000);
-		System.out.println("Estimated Run Time: " +estimatedRunTime/1000);
-		System.out.println("Normalizing Run Time: " +normalizingRunningTime);
-		
+		convertToDoubleArray(runningTimeList);
 		return normalizedRunningTime;
 	}
 	
-	public double[] convertToArray(ArrayList<Double> arrayList) {
+	public long[] convertToLongArray(ArrayList<Long> arrayList) {
+		long[] result = new long[arrayList.size()];
+    	int i = 0;
+    	for(Long l : arrayList) {
+    		result[i++] = l.longValue();
+    	}
+    	return result;
+	}
+	
+	public double[] convertToDoubleArray(ArrayList<Double> arrayList) {
 		double[] result = new double[arrayList.size()];
     	int i = 0;
-    	for(Double d : arrayList) {
-    		result[i++] = d.doubleValue();
+    	for(Double l : arrayList) {
+    		result[i++] = l.doubleValue();
     	}
     	return result;
 	}
@@ -101,7 +121,7 @@ public class PlotData
 	}
 	
 	public static void main(String[] args) {
-		PlotData data = new PlotData(25, 254);
-		data.calculateNodesArray();
+		PlotData data = new PlotData(2, 256);
+		data.calculateTraversalTime();
 	}
 }
